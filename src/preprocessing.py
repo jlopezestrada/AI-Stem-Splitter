75e413e009836d1b450eed789bee103a1ff1e6ce
José Ángel López <jose.a.l.e@hotmail.com>
2023-12-03 15:12:16 +0100

    Feature: new duration parameter and improved logging in preprocessing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,7 +1,8 @@
 import os
-import numpy as np
 import librosa
 import stempeg
+import numpy as np
+from datetime import datetime
 
 # Define base paths
 musdb_path = os.path.join('data', 'raw')
@@ -9,10 +10,10 @@ base_output_path = os.path.join('data', 'processed')
 
 # Processing parameters
 sample_rate = 22050
-segment_length = 3
+segment_length = 5
 hop_length = 512
 
-def extract_and_preprocess(track, output_dir):
+def extract_and_preprocess(track, output_dir, duration=None):
     """
     Extract stems from a given track and preprocess them.
 
@@ -23,6 +24,9 @@ def extract_and_preprocess(track, output_dir):
 
     Parameters:
     track (str): Path to the music track file.
+    output_dir (str): Output directory where feature data are going to be saved.
+    duration (float, optional): Duration in seconds to process from each stem. 
+                                If None, process the entire stem.
 
     Returns:
     None
@@ -41,30 +45,37 @@ def extract_and_preprocess(track, output_dir):
     # Process each stem
     for i in range(stems.shape[0]):
         stem = stems[i]
+        print(f"{datetime.now()} - STEM {i}: {track}")
 
-        # Split stem into segments
-        num_segments = int(np.ceil(len(stem) / (sample_rate * segment_length)))
+        # Calculate the number of segments to process
+        total_duration = len(stem) / sample_rate
+        process_duration = duration if duration is not None else total_duration
+        num_segments = int(np.ceil(process_duration * sample_rate / (sample_rate * segment_length)))
+        print(f"{datetime.now()} - \tNumber of segments: {num_segments}")
         for j in range(num_segments):
             start = j * sample_rate * segment_length
-            end = start + sample_rate * segment_length
+            end = min(start + sample_rate * segment_length, len(stem))
             segment = stem[start:end]
 
             # Extract features, in this case MFCC
+            print(f"{datetime.now()} - \tProcessing feature: {track}")
             mfcc = librosa.feature.mfcc(y=segment, sr=sample_rate, hop_length=hop_length)
             feature_file = os.path.join(output_dir, f'{os.path.basename(track)}_stem{i}_segment{j}.npy')
             np.save(feature_file, mfcc)
-
-def main():
+            print(f"{datetime.now()} - \tProcessed feature: {track}")
+            
+def main(duration=None):
     """
     Process each audio track in the musdb_path directory.
 
     Parameters:
-    None
+    duration (float, optional): Duration in seconds to process from each track. 
+                                If None, process the entire track.
 
     Returns:
     None
     """
-    for root, dir, files in os.walk(musdb_path):
+    for root, dirs, files in os.walk(musdb_path):
         for file in files:
             track_path = os.path.join(root, file)
 
@@ -72,9 +83,10 @@ def main():
             relative_path = os.path.relpath(root, musdb_path)
             output_dir = os.path.join(base_output_path, relative_path)
 
-            # Process each track
-            extract_and_preprocess(track_path, output_dir)
+            # Process each track with the specified duration
+            extract_and_preprocess(track_path, output_dir, duration)
             
+            # Uncomment for debugging
             # print(track_path, output_dir)
 
 if __name__ == '__main__':
